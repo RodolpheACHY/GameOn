2d82c80f1215ef22d685245440f92a224380c0bb
RodolpheACHY <rod23_reseaux@yahoo.fr>
2024-08-26 01:24:30 +0200

    Fix certain problems date but still prb message vous etes trop jeune

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,9 +15,9 @@ const closeBtn = document.querySelector(".close");
 const navLink = document.querySelector("#nav");
 const form = document.querySelector('form[name="reserve"]');
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
 
+form.addEventListener("input",  () => {
+ 
   // Vérification du prénom
   const prenomElement = document.getElementById("first");
   const prenom = prenomElement.value;
@@ -59,12 +59,12 @@ form.addEventListener("submit", function (event) {
   }
   
   // Vérification du format de la date de naissance saisie
-  const validDateNaiss = validerDateNaiss(dateNaiss);
+  /* const validDateNaiss = validerDateNaiss(dateNaiss);
   if (validDateNaiss === false) {
     setErrorElement(dateNaissanceElement);
   } else {
     resetErrorElement(dateNaissanceElement);
-  }
+  } */
 
   // Vérification de l'âge du participant
   dateNaiss = new Date(dateNaissanceElement.value);
@@ -101,6 +101,112 @@ form.addEventListener("submit", function (event) {
     resetErrorElement(accepterConditionsElement);
   }
 
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+   // Initialiser le tableau des erreurs
+  const errors = [];
+  // Vérification du prénom
+  const prenomElement = document.getElementById("first");
+  const prenom = prenomElement.value;
+  const validPrenom = validerPrenom(prenom);
+  if (validPrenom === false) {
+    errors.push("Le prénom doit contenir au moins 2 caractères.");
+    setErrorElement(prenomElement);
+  } else {
+    resetErrorElement(prenomElement);
+  }
+
+  // Vérification du nom
+  const nomElement = document.getElementById("last");
+  const nom = nomElement.value;
+  const validNom = validerNom(nom);
+  if (validNom === false) {
+    errors.push("Le nom doit contenir au moins 2 caractères.");
+    setErrorElement(nomElement);
+  } else {
+    resetErrorElement(nomElement);
+  }
+
+  // Vérification de l'email
+  const emailElement = document.getElementById("email");
+  const email = emailElement.value;
+  const validEmail = validerEmail(email);
+  if (validEmail === false) {
+    errors.push("L'email n'est pas valide.");
+    setErrorElement(emailElement);
+  } else {
+    resetErrorElement(emailElement);
+  }
+  
+  // Vérification si le champ est vide
+  const dateNaissanceElement = document.getElementById("birthdate");
+  let dateNaiss = new Date(dateNaissanceElement.value);
+  const validDateNaissNonVide = validerChampsNonVide(dateNaiss);
+  if (validDateNaissNonVide === false) {
+    errors.push("La date de naissance est obligatoire.");
+    setErrorElement(dateNaissanceElement);
+  } else {
+    resetErrorElement(dateNaissanceElement);
+  }
+  
+  // Vérification du format de la date de naissance saisie
+  /* const validDateNaiss = validerDateNaiss(dateNaiss);
+  if (validDateNaiss === false) {
+    errors.push("La date de naissance n'est pas valide.");
+    setErrorElement(dateNaissanceElement);
+  } else {
+    resetErrorElement(dateNaissanceElement);
+  } */
+
+  // Vérification de l'âge du participant
+  dateNaiss = new Date(dateNaissanceElement.value);
+  const validDateNaiss18 = valider18ans(dateNaiss);
+  if (validDateNaiss18 === false) {
+    errors.push("Vous devez avoir au moins 18 ans pour participer au concours.");
+    setErrorTooYoungElement(dateNaissanceElement);
+  } else {
+    resetErrorTooYoungElement(dateNaissanceElement);
+  } 
+
+  // Vérification du nombre de concours auxquels le user a déjà participé 
+  const nbConcoursElement = document.getElementById("quantity");
+  const nbConcours = nbConcoursElement.value;
+  const validNbConcours = validerNbConcours(nbConcours);
+  if (validNbConcours === false) {
+    errors.push("Le nombre de concours doit être un nombre entier positif.");
+    setErrorElement(nbConcoursElement);
+  } else {
+    resetErrorElement(nbConcoursElement);
+  }
+
+  // Vérification de la ville sélectionnée
+  const listBtnRadiovilleElement =  document.getElementById("choixVille"); 
+  if (validerBtnRadioVille() === false) {
+    errors.push("Vous devez sélectionner une ville.");
+    setErrorElement(listBtnRadiovilleElement);
+  } else {
+    resetErrorElement(listBtnRadiovilleElement);
+  }
+  
+  // Vérification de la case à cocher pour confirmer l'acceptation des conditions d'utilisation
+  const accepterConditionsElement = document.getElementById("checkbox1");
+  if (validerAccepterConditions() === false) {
+    errors.push("Vous devez accepter les conditions d'utilisation.");
+    setErrorElement(accepterConditionsElement);
+  } else {
+    resetErrorElement(accepterConditionsElement);
+  }
+
+  // Si le tableau des erreurs n'est pas vide, on affiche les erreurs 
+  if (errors.length > 0) {
+    alert(errors.join("\n"));
+  } else {
+    closeModal();  // On ferme la popup
+    alert("votre formulaire a été envoyé avec succès !");
+  }
 });
 
 function setErrorElement(input) {
@@ -156,7 +262,8 @@ function closeModal() {
  * @throws {Error}
  */
 function validerPrenom(prenom) {
-  if (prenom.length > 2) {
+   // Initialiser le tableau des erreurs
+  if (prenom.length >= 2) {
     return true;
   }
     return false;
@@ -169,7 +276,7 @@ function validerPrenom(prenom) {
  * @throws {Error}
  */
 function validerNom(nom) {
-  if (nom.length > 2) {
+  if (nom.length >= 2) {
     return true;
   }
     return false;
@@ -194,7 +301,8 @@ function validerEmail(email) {
  * @param {Date} dateNaiss
  */
 function validerChampsNonVide(dateNaiss) {
-  if (!dateNaiss || dateNaiss === "jj/mm/aaaa" || dateNaiss === "") {
+  // if (!dateNaiss || dateNaiss === "jj/mm/aaaa" || dateNaiss === "") {
+  if (!dateNaiss) {
     return false; // Le champ est vide
   } else {
     return true;
@@ -209,11 +317,13 @@ function validerChampsNonVide(dateNaiss) {
 function valider18ans(dateNaiss) {
   const maintenant = new Date();
   const ageMinimum = 18;
-  let age = maintenant.getFullYear() - dateNaiss.getFullYear();
-  const moisDifference = maintenant.getMonth() - dateNaiss.getMonth();
-  console.log(dateNaiss);
-  if (moisDifference < 0 || (moisDifference === 0 && maintenant.getDate() < dateNaiss.getDate())) {
-    age--;
+  const date = new Date(dateNaiss);
+  //let dateNaissFormated = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  //console.log(dateNaissFormated);
+  let age = maintenant.getFullYear() - date.getFullYear();
+  const moisDifference = maintenant.getMonth() - date.getMonth();
+  if (moisDifference < 0 || (moisDifference === 0 && maintenant.getDate() < date.getDate())) {
+    age--; // On n'a pas encore atteint l'anniversaire
   }  
   return age >= ageMinimum;
 }
@@ -223,22 +333,24 @@ function valider18ans(dateNaiss) {
  * ici : deux caractères au minimum
  * @param {Date} dateNaiss
  */
-function validerDateNaiss(dateNaiss) {
+
+/*function validerDateNaiss(dateNaiss) {
+  //const date = new Date(dateNaiss);
+  //let dateNaissFormated = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
   // Regex pour valider le format JJ/MM/AAAA
-  const dateRegex = new RegExp("^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$");
-  console.log(dateNaiss);
-  if (!dateRegex.test(dateNaiss)) {
+   const dateRegex = new RegExp("^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$");
+  if (!dateRegex.test(date)) {
       return false; // Format incorrect
   }
 
   const [jour, mois, annee] = dateNaiss.split('/').map(Number);
-  dateNaiss = new Date(annee, mois - 1, jour); // mois - 1 car les mois en JS sont 0-indexés
+  date = new Date(annee, mois - 1, jour); // mois - 1 car les mois en JS sont 0-indexés
   // Vérifie que la date est valide (par exemple, pas de 30 février)
   if (dateNaiss.getDate() !== jour || dateNaiss.getMonth() !== mois - 1 || dateNaiss.getFullYear() !== annee) {
       return false; // Date non valide
   }
       return true;
-}
+} */
 
 /**
  * Cette fonction prend un string en paramètre et valide qu'il est au bon format.
@@ -298,24 +410,4 @@ function validerPrchEvenmnt() {
   let balisePrchEvenmnt = document.getElementById("checkbox2");
   let PrchEvenmnt = balisePrchEvenmnt.checked;
   console.log(PrchEvenmnt); // affiche true ou false
-}
-
-/**
- * Cette fonction affiche le message d'erreur passé en paramètre.
- * Si le span existe déjà, alors il est réutilisé pour ne pas multiplier
- * les messages d'erreurs.
- * @param {string} message
- */
-function afficherMessageErreur(message) {
-  let spanErreurMessage = document.getElementById("erreurMessage");
-
-  if (!spanErreurMessage) {
-    // on va ajouter la span dans une div qui a une classe popup
-    let popup = document.querySelector(".popup");
-    spanErreurMessage = document.createElement("span");
-    spanErreurMessage.id = "erreurMessage";
-    popup.append(spanErreurMessage);
-  }
-
-  spanErreurMessage.innerText = message;
 }
